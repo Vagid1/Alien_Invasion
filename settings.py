8de1b8b66ffb6618b18c4756192a4074e615328e
Vagid <vagidbedalov@gmail.com>
2025-01-08 21:22:24 +0300

    settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Класс для хранения всекх настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана.
+        self.screen_width = 1440
+        self.screen_heigth = 800
+        self.bg_color = (230, 230, 230)
