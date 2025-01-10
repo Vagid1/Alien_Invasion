174451814ca0d1cd9ad04d4b1b163a7bda32240e
Vagid <vagidbedalov@gmail.com>
2025-01-10 20:15:36 +0300

    ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,13 +2,17 @@ import sys
 
 import pygame
 
+from settings import  Settings
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
     def __init__(self):
         """Инициализирует игру и создаёт игровые ресурсы."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1440, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_heigth))
         pygame.display.set_caption("Alien Invasion")
         # Назначение цвета фона.
         self.bg_color = (230, 230, 230)
@@ -21,7 +25,7 @@ class AlienInvasion:
                     if event.type == pygame.QUIT:
                         sys.exit()
             #При каждом проходе цикла перерисовывается экран.
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
             # Отображение последнего прорисованного экрана.
                 pygame.display.flip()
 
